c93f2b9798b6498fb8bea586bb295467e4d53329
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-01-24 16:50:21 +0530

    odd.C

---
+++ odd.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter The Numbers");
+ scanf("%d%d",&a,&b);
+ if(a%2==0)
+ printf("The Number is Even");
+ else
+ printf("The Number is Odd");
+}
